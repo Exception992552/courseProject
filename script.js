6b09c856aa0afdebcb5f14dbe630edd8ba537122
Serhii <example@gmail.com>
2025-10-28 19:44:44 +0200

    Practice#1

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(1);
+"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const latestFilm = prompt("Один из последних просмотренных фильмов?", "");
+    const scoreFilm = +prompt("На сколько оцените его?", "");
+    personalMovieDB['movies'][latestFilm] = scoreFilm;
+
+}
